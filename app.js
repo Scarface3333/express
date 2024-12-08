70de7d26e9046e1e93a5e1a3f855ccc4c0626436
Scarface3333 <scarface2005@icloud.com>
2024-12-09 00:08:49 +0300

    ld

--- app.js
+++ app.js
@@ -29,10 +29,6 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// //на всех интерфейсах
-// app.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-//   console.log('Server running on port 8080');
-// });
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -46,9 +42,6 @@ app.use(function(err, req, res, next) {
       status: err.status || 500,
     },
   });
-  require('dotenv').config();
-
-
 });
 
 module.exports = app;
